514e3cc1f69017034b88ef86748fd06859d59e61
Francesco Apruzzese <cescoap@gmail.com>
2012-10-31 10:31:06 +0100

    [IMP] Totale Credito sul partner in base ai dati di vendita al banco

--- partner/partner.py
+++ partner/partner.py
@@ -16,10 +16,22 @@ class res_partner(osv.osv):
 	_name = "res.partner"
 	_inherit = "res.partner"
 
+	def _vb_credit(self, cr, uid, ids, name, arg, context=None):
+		res = {}
+		vb_obj = self.pool.get('vendita_banco')
+		for id in ids:
+			tot = 0.0
+			vb_ids = vb_obj.search(cr, uid, [('state', '=', 'draft'), ('partner_id', '=', id)])
+			if vb_ids:
+				vbs = vb_obj.browse(cr, uid, vb_ids)
+				for vb in vbs:
+					tot += vb.totale
+			res[id] = tot
+		return res
+
 	_columns = {
-		#'genera_ddt' : fields.boolean('Genera DDT',
-		#	help="Indica se il cliente generalmente riceve un DDT al momento della vendita dei prodotti"),
 		'causale' : fields.many2one('vendita.causali', 'Causale', help="Causale predefinita da usare nella vendita al banco"),
+		'vb_credit': fields.function(_vb_credit, string='Totale Credito', multi=False, store=False),
 	}
 
 res_partner()
