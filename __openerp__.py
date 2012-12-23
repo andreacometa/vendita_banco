bab636c4638bb372f4e4b9e506145fb36e914e68
Andrea Cometa <info@andreacometa.it>
2012-12-23 10:36:20 +0100

    [fix] little bug

--- __openerp__.py
+++ __openerp__.py
@@ -1,11 +1,23 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Personalizzazione realizzata da Andrea Cometa
-#    Compatible with OpenERP release 6.0.0
-#    Copyright (C) 2010 Andrea Cometa. All Rights Reserved.
-#    Email: info@andreacometa.it
-#    Web site: http://www.andreacometa.it
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2012 Andrea Cometa All Rights Reserved.
+#                       www.andreacometa.it
+#                       openerp@andreacometa.it
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -23,9 +35,9 @@
     "depends" : ['account', 'invoice_immediata_differita', 'stock', 'l10n_it', 'l10n_it_sale', 'recupero_protocolli',],
     "update_xml" : [
         'stock/stock_view.xml',
-        'vendita_banco/vendita_banco_view.xml',
         'security/vendita_banco_security.xml',
         'security/ir.model.access.csv',
+        'vendita_banco/vendita_banco_view.xml',
         'partner/partner_view.xml',
         'vendita_banco/causali.xml',
         'wizard/wizard_genera_fatture_view.xml',
